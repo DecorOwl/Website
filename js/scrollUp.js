711cc6cbf67f20e0c583de41d94fe1d0184f9acc
kdv4 <vaishnanikishanv1@gmail.com>
2020-06-07 12:06:02 +0530

    Create scrollUp.js

---
+++ js/scrollUp.js
@@ -0,0 +1,27 @@
+/*----------------------------------------------------*/
+/*Scroll Up floationg button*/
+/*----------------------------------------------------*/
+/*const btnScrollToTop=document.querySelector("scrollUp");
+btnScrollToTop.addEventListener("click",function()
+{
+	window.scrollTo(0, 0); 
+	$("html, body").animate({scrollTop:0},"slow");
+});*/
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	mybutton.style.display = "block";
+	}else {
+		mybutton.style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
