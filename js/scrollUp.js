84599b598d5b9e567188c71a070646d64d2c260c
kdv4 <vaishnanikishanv1@gmail.com>
2020-07-18 11:37:25 +0530

    Update scrollUp.js

--- js/scrollUp.js
+++ js/scrollUp.js
@@ -1,7 +1,3 @@
-/*----------------------------------------------------*/
-/*Scroll Up floationg button*/
-/*----------------------------------------------------*/
-
 var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
